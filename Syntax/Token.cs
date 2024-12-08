8e03669abab015e0865b0eebc059ad09166814a2
R-unic <roleyspersonal@gmail.com>
2024-12-08 12:36:53 -0500

    ref: convert IToken into class

---
+++ Syntax/Token.cs
@@ -0,0 +1,10 @@
+﻿namespace Heir.Syntax
+{
+    public class Token(SyntaxKind syntax, string text, object value, Location location)
+    {
+        SyntaxKind Syntax { get; } = syntax;
+        string Text { get; } = text;
+        object Value { get; } = value;
+        Location Location { get; } = location;
+    }
+}
