d6efbb80e26e07daf4a3973b5c12097b04b29818
R-unic <roleyspersonal@gmail.com>
2024-12-08 12:26:33 -0500

    feat: create SyntaxType enum

---
+++ Syntax/SyntaxKind.cs
@@ -0,0 +1,43 @@
+﻿namespace Heir.Syntax
+{
+    public enum SyntaxKind
+    {
+        LParen,
+        RParen,
+        LBracket,
+        RBracket,
+        LBrace,
+        RBrace,
+        Equals,
+        EqualsEquals,
+        BangEquals,
+        Bang,
+        Dot,
+        Question,
+        QuestionQuestion,
+
+        Plus,
+        PlusEqual,
+        PlusPlus,
+        Minus,
+        MinusEqual,
+        MinusMinus,
+        Star,
+        StarEqual,
+        Slash,
+        SlashEqual,
+        SlashSlash,
+        SlashSlashEqual,
+        Percent,
+        PercentEqual,
+        Carat,
+        CaratEqual,
+        
+        Identifier,
+        IntLiteral,
+        FloatLiteral,
+        StringLiteral,
+        BoolLiteral,
+        NoneLiteral
+    }
+}
