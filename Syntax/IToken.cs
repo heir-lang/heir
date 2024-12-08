efb640cdebe813da97f30105e72ce8cf5e64f391
R-unic <roleyspersonal@gmail.com>
2024-12-08 12:35:15 -0500

    feat: create IToken interface & Location class

---
+++ Syntax/IToken.cs
@@ -0,0 +1,10 @@
+﻿namespace Heir.Syntax
+{
+    public interface IToken
+    {
+        SyntaxKind Syntax { get; }
+        object Value { get; }
+        string Text { get; }
+        Location Location { get; }
+    }
+}
---
+++ Syntax/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace Heir.Syntax
+{
+    public class Location(string fileName, uint line, uint column)
+    {
+        string FileName { get; } = fileName;
+        uint Line { get; } = line;
+        uint Column { get; } = column;
+    }
+}
