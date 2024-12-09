616518d16bc054d5436ab40019c1d01bfcfcc44e
R-unic <roleyspersonal@gmail.com>
2024-12-08 19:02:50 -0500

    feat: create Diagnostic & DiagnosticBag classes

---
+++ Diagnostic.cs
@@ -0,0 +1,18 @@
+﻿using Heir.Syntax;
+
+namespace Heir
+{
+    public enum DiagnosticLevel : byte
+    {
+        Warn,
+        Error
+    }
+
+    public class Diagnostic(string code, string message, Location location, DiagnosticLevel level)
+    {
+        public string Code { get; } = code;
+        public string Message { get; } = message;
+        public Location Location { get; } = location;
+        public DiagnosticLevel Level { get; } = level;
+    }
+}
---
+++ DiagnosticBag.cs
@@ -0,0 +1,21 @@
+﻿using Heir.Syntax;
+
+namespace Heir
+{
+    public class DiagnosticBag
+    {
+        private readonly HashSet<Diagnostic> _diagnostics = [];
+
+        public void Warn(string code, string message, Location location)
+        {
+            var diagnostic = new Diagnostic(code, message, location, DiagnosticLevel.Warn);
+            _diagnostics.Add(diagnostic);
+        }
+
+        public void Error(string code, string message, Location location)
+        {
+            var diagnostic = new Diagnostic(code, message, location, DiagnosticLevel.Error);
+            _diagnostics.Add(diagnostic);
+        }
+    }
+}
