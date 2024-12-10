921083e9a35197e5c0b886f5675fdeaf26f630fe
R-unic <roleyspersonal@gmail.com>
2024-12-10 14:38:20 -0500

    feat: create temporary evaluation pipeline function in Program.cs

--- Heir/Program.cs
+++ Heir/Program.cs
@@ -1,21 +1,20 @@
-﻿var lexer = Heir.Lexer.FromFile("./Heir/Test.heir");
-var tokenStream = lexer.GetTokens();
-var parser = new Heir.Parser(tokenStream);
-var ast = parser.Parse();
+﻿Console.WriteLine(evaluateFile("./Heir/Test.heir") ?? "null");
 
-//Console.WriteLine(tokenStream);
-//Console.WriteLine();
-//ast.Display();
-var bytecode = ast.GenerateBytecode();
-//foreach (var instruction in bytecode)
-//    Console.WriteLine(instruction.OpCode + ", " + (instruction.Operand?.ToString() ?? "null"));
+object? evaluateFile(string filePath)
+{
+    var lexer = Heir.Lexer.FromFile(filePath);
+    var tokenStream = lexer.GetTokens();
+    var parser = new Heir.Parser(tokenStream);
+    var ast = parser.Parse();
+    var bytecode = ast.GenerateBytecode();
+    var vm = new Heir.VirtualMachine(parser.Diagnostics, bytecode);
+    var result = vm.Evaluate();
+    Console.WriteLine(result ?? "null");
+    Console.WriteLine();
 
-//Console.WriteLine();
+    Console.WriteLine("Diagnostics:");
+    foreach (var diagnostic in vm.Diagnostics)
+        Console.WriteLine($"{diagnostic.StartLocation} [{diagnostic.Code}] - {diagnostic.Message}");
 
-var vm = new Heir.VirtualMachine(parser.Diagnostics, bytecode);
-Console.WriteLine(vm.Evaluate() ?? "null");
-Console.WriteLine();
-
-Console.WriteLine("Diagnostics:");
-foreach (var diagnostic in lexer.Diagnostics)
-    Console.WriteLine($"{diagnostic.StartLocation} [{diagnostic.Code}] - {diagnostic.Message}");
+    return result;
+}
