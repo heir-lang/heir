ead237e9fb51de2d445837ffbb2c16f2f8d3ba4d
R-unic <roleyspersonal@gmail.com>
2024-12-10 13:14:33 -0500

    feat: create Instruction class

---
+++ Heir/CodeGeneration/Instruction.cs
@@ -0,0 +1,10 @@
+﻿namespace Heir.CodeGeneration
+{
+    public class Instruction(OpCode opCode, int? operand = null)
+    {
+        public OpCode OpCode { get; } = opCode;
+        public int? Operand { get; } = operand;
+
+        public override string ToString() => Operand.HasValue ? $"{OpCode} {Operand}" : OpCode.ToString();
+    }
+}
