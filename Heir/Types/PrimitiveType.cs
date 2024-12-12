f6d7e162400e525329dc838c116cb76571b3eff0
R-unic <roleyspersonal@gmail.com>
2024-12-11 20:51:18 -0500

    feat: create PrimitiveType type class

---
+++ Heir/Types/PrimitiveType.cs
@@ -0,0 +1,31 @@
+﻿namespace Heir.Types
+{
+    public enum PrimitiveTypeKind
+    {
+        Int,
+        Float,
+        String,
+        Char,
+        Bool,
+        None
+    }
+
+    public class PrimitiveType(PrimitiveTypeKind primitiveTypeKind) : SingularType(primitiveTypeKind.ToString().ToLower())
+    {
+        public static PrimitiveType FromValue(object? value)
+        {
+            // TODO: do something with _ or "not null" patterns
+            var primitiveTypeKind = value switch
+            {
+                long => PrimitiveTypeKind.Int,
+                double => PrimitiveTypeKind.Float,
+                string => PrimitiveTypeKind.String,
+                char => PrimitiveTypeKind.Char,
+                bool => PrimitiveTypeKind.Bool,
+                null => PrimitiveTypeKind.None
+            };
+
+            return new(primitiveTypeKind);
+        }
+    }
+}
--- Heir/Types/SingularType.cs
+++ Heir/Types/SingularType.cs
@@ -6,31 +6,6 @@
         public string Name { get; } = name;
         public BaseType[]? TypeArguments { get; } = typeArguments;
 
-        public static SingularType FromValue(object? value)
-        {
-            switch (value)
-            {
-                case long:
-                    return new SingularType("int");
-                case double:
-                    return new SingularType("float");
-                case bool:
-                    return new SingularType("bool");
-
-                default:
-                    {
-                        // handle array/range/dict types
-
-                        return new SingularType(value!.GetType().Name);
-                    }
-            }
-        }
-
-        public static SingularType FromLiteral(LiteralType literal)
-        {
-            return FromValue(literal.Value);
-        }
-
         public new string ToString(bool colors = false)
         {
             return Name + (TypeArguments != null ? $"<{string.Join(", ", TypeArguments.Select(t => t.ToString(colors)))}>" : "");
