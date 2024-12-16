82ceb1df5ddc99c0c7265aa40df7380931953222
R-unic <roleyspersonal@gmail.com>
2024-12-15 22:20:37 -0500

    test: binding binary operations

--- Heir.Tests/BinderTest.cs
+++ Heir.Tests/BinderTest.cs
@@ -29,5 +29,48 @@ namespace Heir.Tests
             Assert.Equal(input, literal.Token.Text);
             Assert.Equal(primitiveTypeKind, literal.Type.PrimitiveKind);
         }
+
+        [Theory]
+        [InlineData("true && false", PrimitiveTypeKind.Bool)]
+        [InlineData("8 // 3", PrimitiveTypeKind.Int)]
+        [InlineData("\"a\" + \"b\"", PrimitiveTypeKind.String)]
+        [InlineData("'a' + 'b'", PrimitiveTypeKind.Char)]
+        public void Binds_PrimitiveBinaryOperations(string input, PrimitiveTypeKind returnTypeKind)
+        {
+            var boundTree = Bind(input);
+            var node = boundTree.Statements.First();
+            Assert.IsType<BoundBinaryOp>(node);
+
+            var binaryOp = (BoundBinaryOp)node;
+            Assert.IsType<PrimitiveType>(binaryOp.Left.Type);
+            Assert.IsType<PrimitiveType>(binaryOp.Right.Type);
+            Assert.IsType<PrimitiveType>(binaryOp.Type);
+
+            var returnType = (PrimitiveType)binaryOp.Type;
+            Assert.Equal(returnTypeKind, returnType.PrimitiveKind);
+        }
+
+        [Theory]
+        [InlineData("7 / 4")]
+        public void Binds_UnionBinaryOperations(string input)
+        {
+            var boundTree = Bind(input);
+            var node = boundTree.Statements.First();
+            Assert.IsType<BoundBinaryOp>(node);
+
+            var binaryOp = (BoundBinaryOp)node;
+            Assert.IsType<PrimitiveType>(binaryOp.Left.Type);
+            Assert.IsType<PrimitiveType>(binaryOp.Right.Type);
+            Assert.IsType<UnionType>(binaryOp.Type);
+
+            var returnType = (UnionType)binaryOp.Type;
+            var intType = returnType.Types.First();
+            var floatType = returnType.Types.Last();
+            Assert.IsType<PrimitiveType>(intType);
+            Assert.IsType<PrimitiveType>(floatType);
+            Assert.Equal(PrimitiveTypeKind.Int, ((PrimitiveType)intType).PrimitiveKind);
+            Assert.Equal(PrimitiveTypeKind.Float, ((PrimitiveType)floatType).PrimitiveKind);
+            Assert.Equal(TypeKind.Union, returnType.Kind);
+        }
     }
 }
