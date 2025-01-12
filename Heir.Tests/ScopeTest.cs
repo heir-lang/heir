bcf5c8974aa3e7eec7051f2bfbee5608b56beea6
R-unic <roleyspersonal@gmail.com>
2025-01-12 12:37:25 -0500

    test: scope assigning & assigning at level

--- Heir.Tests/ScopeTest.cs
+++ Heir.Tests/ScopeTest.cs
@@ -3,10 +3,60 @@ namespace Heir.Tests;
 public class ScopeTest
 {
     [Fact]
+    public void LooksUpVariables()
+    {
+        var scope = new Scope();
+        var localScope = new Scope(scope);
+        const string name = "x";
+        const int value = 123;
+        
+        scope.Define(name, value);
+        Assert.Equal(value, scope.Lookup(name));
+        Assert.True(localScope.IsDeclared(name));
+        Assert.True(localScope.IsDefined(name));
+        Assert.Equal(value, localScope.Lookup(name));
+    }
+    
+    [Fact]
+    public void AssignsVariables()
+    {
+        var scope = new Scope();
+        var localScope = new Scope(scope);
+        const string name = "x";
+        const int value = 123;
+        const int newValue = 420;
+        
+        scope.Define(name, value);
+        scope.Assign(name, newValue);
+        Assert.Equal(newValue, scope.Lookup(name));
+        Assert.True(localScope.IsDeclared(name));
+        Assert.True(localScope.IsDefined(name));
+        Assert.Equal(newValue, localScope.Lookup(name));
+    }
+    
+    [Fact]
+    public void AssignsVariablesAtLevel()
+    {
+        var scope = new Scope();
+        var localScope = new Scope(scope);
+        const string name = "x";
+        const int value = 123;
+        const int newValue = 420;
+        
+        scope.Define(name, value);
+        localScope.AssignAt(name, newValue, 1);
+        Assert.Equal(newValue, scope.Lookup(name));
+        Assert.True(localScope.IsDeclared(name));
+        Assert.True(localScope.IsDefined(name));
+        Assert.Equal(newValue, localScope.Lookup(name));
+    }
+    
+    [Fact]
     public void DeclaresVariables()
     {
         var scope = new Scope();
-        var name = "x";
+        const string name = "x";
+        
         scope.Define(name, null);
         Assert.True(scope.IsDeclared(name));
         Assert.False(scope.IsDefined(name));
@@ -17,24 +67,12 @@ public class ScopeTest
     public void DefinesVariables()
     {
         var scope = new Scope();
-        var name = "x";
-        var value = 123;
-        scope.Define(name, 123);
+        const string name = "x";
+        const int value = 123;
+        
+        scope.Define(name, value);
         Assert.True(scope.IsDeclared(name));
         Assert.True(scope.IsDefined(name));
         Assert.Equal(value, scope.Lookup(name));
-    }
-
-    [Fact]
-    public void LooksUpVariables()
-    {
-        var scope = new Scope();
-        var localScope = new Scope(scope);
-        var name = "x";
-        var value = 123;
-        scope.Define(name, 123);
-        Assert.True(localScope.IsDeclared(name));
-        Assert.True(localScope.IsDefined(name));
-        Assert.Equal(value, localScope.Lookup(name));
     }
 }
