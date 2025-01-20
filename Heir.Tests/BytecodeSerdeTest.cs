0627f4f8d267ed962d0c95c157ae5c35879393c4
R-unic <roleyspersonal@gmail.com>
2025-01-20 18:40:12 -0500

    test: proc instruction ser/de

--- Heir.Tests/BytecodeSerdeTest.cs
+++ Heir.Tests/BytecodeSerdeTest.cs
@@ -5,13 +5,13 @@ namespace Heir.Tests;
 public class BytecodeSerdeTest
 {
     [Fact]
-    public void Serde_BasicInstructions()
+    public void Serde_Proc()
     {
         var bytecode = new Bytecode([
-            new(null, OpCode.PUSH, 6),
-            new(null, OpCode.PUSH, 12),
-            new(null, OpCode.ADD),
-            new(null, OpCode.POP)
+            new(null, OpCode.PROC, new List<Instruction>([
+                new(null, OpCode.PUSH, 69),
+                new(null, OpCode.POP)
+            ]))
         ]);
 
         using var stream = new MemoryStream();
@@ -19,13 +19,40 @@ public class BytecodeSerdeTest
         stream.Position = 0;
             
         var deserializedBytecode = BytecodeDeserializer.Deserialize(stream);
+        AssertBytecodeEqual(bytecode, deserializedBytecode);
+    }
+
+    [Fact]
+    public void Serde_BasicInstructions()
+    {
+        var bytecode = new Bytecode([
+            new(null, OpCode.PUSH, 6),
+            new(null, OpCode.PUSH, 12),
+            new(null, OpCode.ADD),
+            new(null, OpCode.POP),
+            new(null, OpCode.PUSH, "abc")
+        ]);
+
+        using var stream = new MemoryStream();
+        BytecodeSerializer.Serialize(bytecode, stream, true);
+        stream.Position = 0;
+            
+        var deserializedBytecode = BytecodeDeserializer.Deserialize(stream);
+        AssertBytecodeEqual(bytecode, deserializedBytecode);
+    }
+    
+    private static void AssertBytecodeEqual(Bytecode bytecode, Bytecode deserializedBytecode)
+    {
         for (var i = 0; i < bytecode.Count; i++)
         {
             var instruction = bytecode[i];
             var deserializedInstruction = deserializedBytecode.Instructions.ElementAtOrDefault(i);
             Assert.NotNull(deserializedInstruction);
             Assert.Equal(instruction.OpCode, deserializedInstruction.OpCode);
-            Assert.Equal(instruction.Operand, deserializedInstruction.Operand);
+            if (instruction.Operand is List<Instruction> instructions && deserializedInstruction.Operand is List<Instruction> deserializedInstructions)
+                AssertBytecodeEqual(new(instructions), new(deserializedInstructions));
+            else
+                Assert.Equal(instruction.Operand, deserializedInstruction.Operand);
         }
     }
 }
