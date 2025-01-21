263bf44683ffd6a248bb406911a648dbe7959bf3
R-unic <roleyspersonal@gmail.com>
2025-01-20 22:14:38 -0500

    perf: pre-compute EQ, NEQ, UNM, and BNOT with literals

--- Heir/CodeGeneration/BytecodeOptimizer.cs
+++ Heir/CodeGeneration/BytecodeOptimizer.cs
@@ -36,6 +36,25 @@ public class BytecodeOptimizer(List<Instruction> bytecode, DiagnosticBag diagnos
                 // pre-compute the result of operations using only literal values
                 // FUCK THIS CODE
                 
+                // unary
+                {
+                    if (instruction.Operand is long or ulong or int or uint or short or ushort or byte or sbyte or double or float or decimal &&
+                        PeekBytecode(1) is
+                        {
+                            OpCode: OpCode.UNM or OpCode.BNOT
+                        } operation)
+                    {
+                        var result = operation.OpCode == OpCode.UNM
+                            ? -Convert.ToDouble(instruction.Operand)
+                            : ~Convert.ToInt64(instruction.Operand);
+
+                        Advance();
+                        Advance();
+                        return instruction.WithOperand(result);
+                    }
+                }
+                
+                // binary
                 {
                     if (instruction.Operand is long or ulong or int or uint or short or ushort or byte or sbyte or double or float or decimal &&
                         PeekBytecode(1) is
@@ -109,6 +128,29 @@ public class BytecodeOptimizer(List<Instruction> bytecode, DiagnosticBag diagnos
                         var left = Convert.ToBoolean(leftRaw);
                         var right = Convert.ToBoolean(rightRaw);
                         var result = calculate(left, right);
+
+                        Advance();
+                        Advance();
+                        Advance();
+                        return instruction.WithOperand(result);
+                    }
+                }
+                {
+                    if (instruction.Operand is var left &&
+                        PeekBytecode(1) is
+                        {
+                            OpCode: OpCode.PUSH,
+                            Operand: var right
+                        } &&
+                        PeekBytecode(2) is
+                        {
+                            OpCode: OpCode.EQ or OpCode.NEQ
+                        } operation)
+                    {
+                        var equalityComparer = EqualityComparer<object>.Default;
+                        var result = equalityComparer.Equals(left, right);
+                        if (operation.OpCode == OpCode.NEQ)
+                            result = !result;
 
                         Advance();
                         Advance();
