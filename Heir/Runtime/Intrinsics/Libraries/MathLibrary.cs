b58125440c02bdfccf6c6e8cfb66760de88b4fb8
R-unic <roleyspersonal@gmail.com>
2025-01-21 19:40:16 -0500

    feat: add math.e, math.tau, and math.inf constants

--- Heir/Runtime/Intrinsics/Libraries/MathLibrary.cs
+++ Heir/Runtime/Intrinsics/Libraries/MathLibrary.cs
@@ -7,10 +7,16 @@ public class MathLibrary() : IntrinsicLibrary("math", _type)
 {
     private static readonly InterfaceType _type = InterfaceType.Readonly("math", new()
     {
-        { "pi", PrimitiveType.Float }
+        { "pi", PrimitiveType.Float },
+        { "e", PrimitiveType.Float },
+        { "tau", PrimitiveType.Float },
+        { "inf", PrimitiveType.Float }
     });
 
     public override object? Value { get; } = new ObjectValue([
         new("pi", Math.PI),
+        new("e", Math.E),
+        new("tau", Math.Tau),
+        new("inf", double.PositiveInfinity)
     ]);
 }
